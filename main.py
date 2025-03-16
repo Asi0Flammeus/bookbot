5c68366b9f9939c57495be3b2f4760652e8b8c66
Asi0Flammeus <asi0.flammeus@pm.me>
2025-03-16 13:32:10 +0100

    implement sorted report

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-from stats import get_num_words, count_character
+from stats import get_num_words, count_character, sort_dictionnary
 def get_boot_text(filepath):
     with open(filepath) as f:
         content = f.read()
     return content
 
 
+def print_report(filepath, num_words, sorted_dictionnary):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for character_dict in sorted_dictionnary:
+        if character_dict["character"].isalpha():
+            print(f"{character_dict['character']}: {character_dict['num']}")
+    print("============= END ==============")
+
 def main():
     filepath = "./books/frankenstein.txt"
     content = get_boot_text(filepath)
     num_words = get_num_words(content)
     character_count = count_character(content)
-    print(f"{num_words} words found in the document")
-    print(character_count)
+    sorted_dictionnary = sort_dictionnary(character_count)
+    print_report(filepath, num_words, sorted_dictionnary)
+
+
 
 main()
--- stats.py
+++ stats.py
@@ -1,15 +1,27 @@
 def get_num_words(content):
     return len(content.split())
 
-
 def count_character(content):
-    words = content.split()
     character_count = {}
-    for word in words:
-        for letter in word:
-            letter = letter.lower()
-            if letter in character_count:
-                character_count[letter] += 1
-            else:
-                character_count[letter] = 1
+    for letter in content:
+        letter = letter.lower()
+        if letter in character_count:
+            character_count[letter] += 1
+        else:
+            character_count[letter] = 1
     return character_count
+
+def sort_on(dict):
+    return dict["num"]
+
+def sort_dictionnary(character_count):
+    sorted_character_count = []
+
+    for key in character_count:
+        temp_dict = {"character": key, "num": character_count[key]}
+        sorted_character_count.append(temp_dict)
+
+    sorted_character_count.sort(reverse=True, key=sort_on)
+
+    return sorted_character_count
+
